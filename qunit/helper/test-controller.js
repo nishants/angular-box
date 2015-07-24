32746c5ed450f31cb3b4a5a270db3f0ae06409dc
nishant <nishant.singh87@gmail.com>
2015-07-24 17:15:16 +0530

    adding test controller.

---
+++ qunit/helper/test-controller.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  // add all pre test setup in constructor i.e. scope.init
+  crawler.app.controller('TestController', function ($scope, server) {
+
+  });
+
+}).call(this);
