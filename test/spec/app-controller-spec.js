c3b024145a769fa8ff1f964ae6a98f6a292a2942
nishant <nishant.singh87@gmail.com>
2015-07-30 13:46:39 +0530

    adding unit test for app controller.

--- test/spec/app-controller-spec.js
+++ test/spec/app-controller-spec.js
@@ -1,19 +1,41 @@
-describe("Landing page : AppController", function() {
+describe("AppController", function () {
 
-  beforeEach(module('app'));
+  var
+      controller,
+      $scope,
+      $location,
+      appService,
+      items = {
+        id: "1",
+        name: "One",
+        description: "first one",
+        date: "00:58:43 - 2015-07-27",
+      },
 
-  var controller,
-      scope;
+      setupAngular = function () {
+        module('app');
+        inject(function (_$rootScope_, _$controller_, _$location_) {
+          $scope = _$rootScope_.$new();
+          $location = _$location_;
+          appService = {
+            getAllItems: function (succes, err) {
+              succes(items);
+            }
+          };
 
-  beforeEach(inject(function ($rootScope, $controller) {
-    scope = $rootScope.$new();
-    controller = $controller('AppController', {
-      $scope: scope
+          spyOn($location, "url")
+
+          controller = _$controller_('AppController', {$scope: $scope, $location: $location, appService: appService});
+        });
+      };
+
+  beforeEach(setupAngular);
+
+  describe('$scope.init', function () {
+    it('fetches static data and redirects to home', function () {
+      $scope.init("abc");
+      expect($location.url).toHaveBeenCalledWith("abc");
+      expect($scope.items).toBe(items);
     });
-  }));
-
-  it('says hello world!', function () {
-    expect(scope.items).toEqual("Hello world!");
   });
-
 });
